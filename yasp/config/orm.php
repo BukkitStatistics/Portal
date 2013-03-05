b699db3d52243eaf5515f2edb87d66610bc8f682
Christian Swan <dazzel@online.de>
2013-03-05 19:23:43 +0100

    add check for db connection
    - add cache for db connection
    - remove cache info file..

--- yasp/config/orm.php
+++ yasp/config/orm.php
@@ -3,6 +3,16 @@ if(defined('DB_DATABASE') && DB_DATABASE != '') {
     try {
         $db = new fDatabase('mysql', DB_DATABASE, DB_USER, DB_PW, DB_HOST);
 
+        if($cacheSingle->get('dbOffline') === null) {
+            $db->connect();
+            $db->close();
+            // cache for 12 hours
+            $cacheSingle->set('dbOffline', false, 60 * 60 * 12);
+        }
+        elseif($cacheSingle->get('dbOffline'))
+            throw new fConnectivityException(fText::compose('Database not reachable. Try again in a few minutes.'));
+
+
         fORMDatabase::attach($db);
         fORM::enableSchemaCaching($cacheSingle);
 
@@ -24,7 +34,16 @@ if(defined('DB_DATABASE') && DB_DATABASE != '') {
 
         // adds prefix
         $db->registerHookCallback('unmodified', Util::addPrefix);
-    } catch(fException $e) {
+    } catch(fNotFoundException $e) {
+        $cacheSingle->set('dbOffline', true, 60 * 2);
+        fMessaging::create('errors', '{default}', $e->getMessage());
+    } catch(fAuthorizationException $e) {
+        $cacheSingle->set('dbOffline', true, 60 * 2);
+        fMessaging::create('errors', '{default}', $e->getMessage());
+    } catch(fConnectivityException $e) {
+        $cacheSingle->set('dbOffline', true, 60 * 2);
         fMessaging::create('errors', '{default}', $e->getMessage());
     }
 }
+else
+    die('db.php config file not setup properly...');
