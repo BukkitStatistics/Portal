513a0c73c66e367ef976bf711e28341fd82e4cdf
Christian Swan <dazzel@online.de>
2013-03-08 13:56:23 +0100

    add distance stats

--- yasp/classes/orm/Player.php
+++ yasp/classes/orm/Player.php
@@ -79,6 +79,31 @@ class Player extends fActiveRecord {
         return 0;
     }
 
+    public static function getDistanceOfType($type) {
+        try {
+            if($type == 'total') {
+                $res = fORMDatabase::retrieve()->translatedQuery('
+                        SELECT SUM(foot) + SUM(minecart) + SUM(pig)
+                        FROM "prefix_distance_players"
+                ');
+            }
+            else {
+                $res = fORMDatabase::retrieve()->translatedQuery('
+                        SELECT SUM(' . $type . ')
+                        FROM "prefix_distance_players"
+                ');
+            }
+
+            return new fNumber($res->fetchScalar());
+        } catch(fNoRowsException $e) {
+            fCore::debug($e->getMessage());
+        } catch(fNoRemainingException $e) {
+            fCore::debug($e->getMessage());
+        } catch(fSQLException $e) {
+            fCore::debug($e->getMessage());
+        }
+    }
+
     public function getUrlName() {
         return fURL::makeFriendly($this->getName());
     }
