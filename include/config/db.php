8ff6d4533ec19f18547c23a524c1f5e340d91306
Christian Swan <dazzel@online.de>
2013-03-19 17:19:58 +0100

    re-add some files

---
+++ include/config/db.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+* Do not modify this unless you know what you are doing!
+*/
+
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PW', '');
+define('DB_DATABASE', '');
+define('DB_PREFIX', '');
