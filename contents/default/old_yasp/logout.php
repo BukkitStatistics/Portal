149151ca0e7e2a72096955b45e27cb83f4ed4d20
Christian Swan <dazzel@online.de>
2013-03-19 17:27:58 +0100

    keep only files important for installation

--- contents/default/old_yasp/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("../include/config/config.php");
-include("../include/security/logout.php");
-
-$origin = $_POST['origin'];
-header("Location: ../".$origin); 
-?>
--- include/classes/orm/DetailedLogPlayer.php
+++
@@ -1,4 +0,0 @@
-<?php
-class DetailedLogPlayer extends fActiveRecord {
-
-}
