8f65121c541817f9c5e81f513f68fc458c11a930
Christian Swan <dazzel@online.de>
2013-03-29 23:44:22 +0100

    fix death log order

--- contents/default/mod/death_log.php
+++ contents/default/mod/death_log.php
@@ -5,12 +5,12 @@ $tpl_death = Util::newTpl($this, 'mod/death_log', 'death_log');
 $death_log_pvp = fRecordSet::build(
     'DetailedPvpKill',
     array(),
-    array('time' => 'asc')
+    array('time' => 'desc')
 );
 $death_log_pve = fRecordSet::build(
     'DetailedPveKill',
     array(),
-    array('time' => 'asc')
+    array('time' => 'desc')
 );
 
 $page = fRequest::get('p', 'int', 1);
@@ -22,7 +22,7 @@ $death_log = fRecordSet::buildFromArray(
     $death_log_pvp->count() + $death_log_pve->count(),
     $limit,
     $page
-)->sort('getTime', 'asc')->slice(($page - 1) * $limit, $limit, true);
+)->sort('getTime', 'desc')->slice(($page - 1) * $limit, $limit, true);
 
 $tpl_death->set('death_log', $death_log);
 
