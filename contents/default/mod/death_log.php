64b80145a4a0e376fd017b500837aa8956b7748a
Christian Swan <dazzel@online.de>
2013-04-16 12:43:50 +0200

    fix death log
    Now the loading time is drastically reduced

--- contents/default/mod/death_log.php
+++ contents/default/mod/death_log.php
@@ -1,28 +1,32 @@
 <?php
 $tpl_death = Util::newTpl($this, 'mod/death_log', 'death_log');
 
+$page = fRequest::get('p', 'int', 1);
+$limit = 10;
+
 // TODO: limit total amount by time
 $death_log_pvp = fRecordSet::build(
     'DetailedPvpKill',
     array(),
-    array('time' => 'desc')
+    array('time' => 'desc'),
+    $limit / 2,
+    $page
 );
 $death_log_pve = fRecordSet::build(
     'DetailedPveKill',
     array(),
-    array('time' => 'desc')
+    array('time' => 'desc'),
+    $limit / 2 + ($limit / 2 - $death_log_pvp->count()),
+    $page
 );
-
-$page = fRequest::get('p', 'int', 1);
-$limit = 10;
 
 $death_log = fRecordSet::buildFromArray(
     array('DetailedPvpKill', 'DetailedPveKill'),
     array_merge($death_log_pve->getRecords(), $death_log_pvp->getRecords()),
-    $death_log_pvp->count() + $death_log_pve->count(),
+    $death_log_pvp->count(true) + $death_log_pve->count(true),
     $limit,
     $page
-)->sort('getTime', 'desc')->slice(($page - 1) * $limit, $limit, true);
+)->sort('getTime', 'desc');
 
 $tpl_death->set('death_log', $death_log);
 
