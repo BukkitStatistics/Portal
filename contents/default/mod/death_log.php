d11a6f0bf9250426bf714f6d1e2d91251f0e7580
Christian Swan <dazzel@online.de>
2013-04-22 18:52:14 +0200

    fix death log fixes YASP-86

--- contents/default/mod/death_log.php
+++ contents/default/mod/death_log.php
@@ -4,31 +4,25 @@ $tpl_death = Util::newTpl($this, 'mod/death_log', 'death_log');
 $page = fRequest::get('p', 'int', 1);
 $limit = 10;
 
-// TODO: limit total amount by time
-$death_log_pvp = fRecordSet::build(
-    'DetailedPvpKill',
-    array(),
-    array('time' => 'desc'),
-    $limit / 2,
-    $page
-);
-$death_log_pve = fRecordSet::build(
-    'DetailedPveKill',
-    array(),
-    array('time' => 'desc'),
-    $limit / 2 + ($limit / 2 - $death_log_pvp->count()),
-    $page
+$death_log = fORMDatabase::retrieve('name:default')->translatedQuery(
+    '
+    SELECT SQL_CALC_FOUND_ROWS material_id, entity_id AS id1, player_id AS id2, time, player_killed
+    FROM prefix_detailed_pve_kills
+    UNION ALL
+    SELECT material_id, player_id AS id1, victim_id AS id2, time, NULL AS player_killed
+    FROM prefix_detailed_pvp_kills
+    ORDER BY time DESC
+    LIMIT %i,%i
+    ',
+    $limit * ($page - 1),
+    $limit
 );
 
-$death_log = fRecordSet::buildFromArray(
-    array('DetailedPvpKill', 'DetailedPveKill'),
-    array_merge($death_log_pve->getRecords(), $death_log_pvp->getRecords()),
-    $death_log_pvp->count(true) + $death_log_pve->count(true),
-    $limit,
-    $page
-)->sort('getTime', 'desc');
+$pages = fORMDatabase::retrieve('name:default')->translatedQuery('SELECT FOUND_ROWS()')->fetchScalar();
 
 $tpl_death->set('death_log', $death_log);
+$tpl_death->set('death_log_page', $page);
+$tpl_death->set('death_log_pages', ceil($pages / $limit));
 
 if(fRequest::isAjax()) {
     $tpl_death->place();
